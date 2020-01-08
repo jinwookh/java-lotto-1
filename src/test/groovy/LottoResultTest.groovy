1d144e7779c90bc227b881fb2d2d0808988910a3
jinwook <jin942002@naver.com>
2020-01-08 22:10:14 +0900

    test: 5등 수익률 parameter test 추가

--- src/test/groovy/LottoResultTest.groovy
+++ src/test/groovy/LottoResultTest.groovy
@@ -23,7 +23,7 @@ class LottoResultTest extends Specification {
         numbers            | winningNumbers        | bonusNumber || result
         [1, 2, 3, 4, 5, 6] | [7, 8, 9, 10, 11, 12] | 16          || 0
         [1, 2, 3, 4, 5, 6] | [1, 2, 3, 4, 5, 7]    | 16          || 150000
-
+        [1, 2, 3, 4, 5, 6] | [1, 2, 3, 7, 8, 9]    | 16          || 500
 
     }
 }
